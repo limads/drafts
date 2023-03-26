c8429846cb5a5d4d091ac4167ffc74a1b7c7e856
Diego Lima <lima.ds@outlook.com>
2023-03-26 18:07:08 -0300

    Updated build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,5 +15,5 @@ cargo build --release --offline --verbose \
     --config "source.\"https://github.com/typst/pixglyph\".git = \"https://github.com/typst/pixglyph\"" \
     --config "source.\"https://github.com/typst/typst\".replace-with = \"vendored-sources\"" \
     --config "source.\"https://github.com/typst/typst\".git = \"https://github.com/typst/typst\"" \
-    --config "source.\"https://github.com/typst/unicode-math-class\".replace-with = \"vendored-sources\""
+    --config "source.\"https://github.com/typst/unicode-math-class\".replace-with = \"vendored-sources\"" \
     --config "source.\"https://github.com/typst/unicode-math-class\".git = \"https://github.com/typst/unicode-math-class\""
