3b31b0a6aaf3033be422dd34270374bc890102d2
Diego Lima <lima.ds@outlook.com>
2023-03-26 18:52:47 -0300

    Updated build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,4 @@
+cargo generate-lockfile --offline
 cargo build --release --offline --verbose \
     --config "source.vendored-sources.directory=\"deps\"" \
     --config "source.crates-io.replace-with = \"vendored-sources\"" \
